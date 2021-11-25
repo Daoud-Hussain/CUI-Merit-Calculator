da05c4f3643ff076256cc5f464d16940caad3ee3
Daoud-Hussain <87219816+Daoud-Hussain@users.noreply.github.com>
2021-11-25 17:49:43 +0500

    Update merit.java

--- merit.java
+++ merit.java
@@ -1,3 +1,5 @@
+//Java Program to check merit of COMSATS University Islamabad.
+
 import java.util.Scanner;
 public class merit{
     public static void main(String[] args) {
@@ -9,44 +11,44 @@ public class merit{
         // condition for both types of candidates
         if (choice == 1){
             // Creating Object for Input
-        	Scanner obj = new Scanner(System.in);
+        	Scanner input = new Scanner(System.in);
 
       	// Taking Inputs from user
         	System.out.print("Enter your Matric Marks: ");
-        	float matric = obj.nextFloat();
+        	float matricMarks = input.nextFloat();
         	System.out.print("Enter your NTS Marks: ");
-        	float nts = obj.nextFloat();
+        	float ntsMarks = input.nextFloat();
 
         // Calculating Percentage
-        	float matric_percentage = (matric/1100)*100;
-        	float nts_percentage = (nts/100)*100;
+        	float matricPercentage = (matricMarks/1100)*100;
+        	float ntsPercentage = (ntsMarks/100)*100;
 
         // Calculating Merit According to the scheme 50% Matric and 50% NTS
-        	double merit_cui = ((matric_percentage*0.5)+(nts_percentage*0.5));
+        	double meritCUI = ((matricPercentage*0.5)+(ntsPercentage*0.5));
 
-        	System.out.printf("Your Aggregate is: %5.2f", merit_cui);
+        	System.out.printf("Your Aggregate is: %5.2f", meritCUI);
         }
     	else if (choice == 2){
     	// Creating Object for Input
-        	Scanner obj = new Scanner(System.in);
+        	Scanner input = new Scanner(System.in);
 
-        // Taking Inputs from user
+      	// Taking Inputs from user
         	System.out.print("Enter your Matric Marks: ");
-        	float matric = obj.nextFloat();
+        	float matricMarks = input.nextFloat();
         	System.out.print("Enter your Inter Marks: ");
-        	float inter = obj.nextFloat();
+        	float interMarks = input.nextFloat();
         	System.out.print("Enter your NTS Marks: ");
-        	float nts = obj.nextFloat();
+        	float ntsMarks = input.nextFloat();
 
         // Calculating Percentage
-        	float matric_percentage = (matric/1100)*100;
-        	float inter_percentage = (inter/1100)*100;
-        	float nts_percentage = (nts/100)*100;
+        	float matricPercentage = (matricMarks/1100)*100;
+        	float interPercentage = (interMarks/1100)*100;
+        	float ntsPercentage = (ntsMarks/100)*100;
 
         // Calculating Merit According to the scheme 10% Matric, 40% Inter and 50% NTS
-        	double merit_cui = ((matric_percentage*0.1)+(inter_percentage*0.4)+(nts_percentage*0.5));
+        	double meritCUI = ((matricPercentage*0.1)+(interPercentage*0.4)+(ntsPercentage*0.5));
 
-        	System.out.printf("Your Aggregate is: %5.2f", merit_cui);
+        	System.out.printf("Your Aggregate is: %5.2f", meritCUI);
     	}
     	else{
     		System.out.println("Invalid choice. Try Again! ");
